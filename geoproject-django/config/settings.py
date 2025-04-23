74fc8bc372ebee198fa695b9bfc027c194e6c4e0
Alexander <sasha.beloglazoov@gmail.com>
2025-04-23 12:26:18 +0300

    update host database

--- geoproject-django/config/settings.py
+++ geoproject-django/config/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", default="localhost"),
+        "HOST": os.getenv("DB_HOST", default="db"),
         "PORT": os.getenv("DB_PORT", default="5432")
     }
 }
